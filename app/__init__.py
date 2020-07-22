8439afbaf744806d61476ddb831cfcded532d87b
Frank <kipfrankline@gmail.com>
2020-07-23 01:42:12 +0300

    Add function that renders 404 page

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
 
-    # initializing
+    # initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
     mail.init_app(app)
@@ -34,5 +34,5 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
 
-        
+ 
     return app
