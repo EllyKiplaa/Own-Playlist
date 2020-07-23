524893190c10f195d08ad18da9d6906ab8b85bac
Maratah <maratah7@gmail.com>
2020-07-23 20:35:12 +0300

    commit coflicting files changes.

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,7 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-<<<<<<< HEAD
-from .config import config_options
-
-# from config import config_options
-=======
 
 from config import config_options
->>>>>>> 0d439dc63e95a26ad0eb619297b36576a26ee001
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
