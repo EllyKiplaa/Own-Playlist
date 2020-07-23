b2e9f3fa619085b66d25f9b3edfad727bfe99ecd
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-23 11:36:20 +0300

    Import views Flask login manager SQLALCHEMY

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,20 @@
 from flask import Flask
-
-# Initializing application
-app = Flask(__name__)
-
 from app import views
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+def create_app(config_name):
+      
+    #Initializing Flask Extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
