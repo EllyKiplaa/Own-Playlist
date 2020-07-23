bd231b6e66c214080458e10217d738f8f1822eed
EllyKiplaa <ellykiplangat200@gmail.com>
2020-07-23 19:37:33 +0300

    Add the testing configs and options also the database URL

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     '''
     
 
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://elly:Access2020@localhost/own_playlist"
     # SECRET_KEY = os.environ.get('SECRET_KEY')
 
  
@@ -16,18 +16,28 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://elly:Access2020@localhost/own_playlist"
     
-
+class TestConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://elly:Access2020@localhost/own_playlist"
+    
 
 class DevConfig(Config):
     '''
     Development  configuration child class
     Args:
     Config: The parent configuration class with General configuration settings
+
     '''
     DEBUG = True
 
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'testing' :TestConfig
 }
