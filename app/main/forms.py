0ac11d31f1c95df6b9a852f76fa191355c9acce9
Frank <kipfrankline@gmail.com>
2020-07-23 16:24:10 +0300

    Add class form to create playlists

---
+++ app/main/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from  wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
+class OwnPlaylistForm(FlaskForm):
+    """
+    Class to create forms for creating playlists
+    """
+    name = StringField("Playlist Name", validators=[Required()])
+    submit = SubmitField("Submit")
